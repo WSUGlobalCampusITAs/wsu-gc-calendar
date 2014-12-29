f6ff72cacc8230232c938e9a47a74915a7b1d3b5
David Lystad <david.lystad@email.wsu.edu>
2014-12-29 07:53:31 -0800

    Adding Thanksgiving calculation

--- Calendar Converter/Calendar Converter/Semester.cs
+++ Calendar Converter/Calendar Converter/Semester.cs
@@ -12,6 +12,8 @@ namespace Calendar_Converter
         private DateTime memSemStart;
         private DateTime memSemEnd;
         private int memNumWeeks;
+        private int memBreakWeek;
+        private List<Week> memSemesterWeeks;
 
         //Constructor for Semester Class
         public Semester(DateTime SemesterStart, int NumberofWeeks)
@@ -19,6 +21,37 @@ namespace Calendar_Converter
             memSemStart = SemesterStart;
             memSemEnd = SemesterStart.AddDays(NumberofWeeks * 7.0);
             memNumWeeks = NumberofWeeks;
+            if(memSemStart.Month <= 11 && memSemEnd.Month >= 11)
+            {
+                memBreakWeek = 1;
+                DateTime BreakFinder = memSemStart;
+                while(BreakFinder.DayOfYear < memSemEnd.DayOfYear)
+                {
+                    if (BreakFinder.DayOfYear >= Thanksgiving(BreakFinder.Year).DayOfYear)
+                    {
+                        break;
+                    }
+                    memBreakWeek++;
+                    BreakFinder.AddDays(7);
+                    
+                }
+                if(memBreakWeek >= NumberofWeeks)
+                {
+                    memBreakWeek = -1;
+                }
+            }
+            else
+            {
+                if(memSemStart.Month <= 3 && memSemEnd.Month >= 3)
+                {
+                    memBreakWeek = 10;
+                }
+            }
+
+            for (int i = 0; i < NumberofWeeks; i++ )
+            {
+
+            }
         }
 
         //Setters and Getters
@@ -29,6 +62,28 @@ namespace Calendar_Converter
         public DateTime SemesterEnd
         {
             get { return memSemEnd; }
+        }
+
+        /// <summary>
+        /// This method was referenced from http://solidcoding.blogspot.com/2007/11/c30-extension-method-to-check-holidays.html
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
+        private static DateTime Thanksgiving(int year)
+        {
+            DateTime thanksgiving = DateTime.MinValue;
+            List<DateTime> november = new List<DateTime>();
+            for (int count = 1; count < 31; count++)
+            {
+                november.Add(new DateTime(year, 11, count));
+            }
+            DayOfWeek thursdays = DayOfWeek.Thursday;
+
+            //Use LINQ query to find all the thursdays of the month of November.
+            //Thanksgiving is on the 4th thursday
+            //Use the combination of Take and Last to get the 4th thursday of the month
+            thanksgiving = (from d in november where d.DayOfWeek == thursdays orderby d.Day ascending select d).Take(4).Last();
+            return thanksgiving;
         }
     }
 }
--- Calendar Converter/Calendar Converter/Week.cs
+++ Calendar Converter/Calendar Converter/Week.cs
@@ -8,5 +8,34 @@ namespace Calendar_Converter
 {
     class Week
     {
+        private DateTime memWeekStart;
+        private DateTime memWeekEnd;
+        private int memWeekNumber;
+        private bool memIsBreak;
+
+        public Week(DateTime SemesterStart, int WeekNumber, bool IsBreak)
+        {
+            memWeekStart = SemesterStart.AddDays(WeekNumber * 7);
+            memWeekEnd = memWeekStart.AddDays(7);
+            memIsBreak = IsBreak;
+            memWeekNumber = WeekNumber;
+        }
+
+        public DateTime WeekStart
+        {
+            get { return memWeekStart; }
+        }
+        public DateTime WeekEnd
+        {
+            get { return memWeekEnd; }
+        }
+        public int WeekNumber
+        {
+            get { return memWeekNumber; }
+        }
+        public bool Break
+        {
+            get { return memIsBreak; }
+        }
     }
 }
