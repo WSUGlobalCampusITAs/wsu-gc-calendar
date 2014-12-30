2f400e7f04f350fb9dced5d6f5df4cc2a7ef7f76
David Lystad <david.lystad@email.wsu.edu>
2014-12-29 16:14:28 -0800

    adding LogicEngine to the system, and redesign.

---
+++ Calendar Converter/Calendar Converter/LogicEngine.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Calendar_Converter
+{
+    /// <summary>
+    /// This class implements the functionality of navigating weeks, and populating the semesters. 
+    /// This is added for the sake of seperating the user interface from the logic of the code. 
+    /// </summary>
+    class LogicEngine : INotifyPropertyChanged
+    {
+        //Member Variable Declarations. 
+        private Week memOldWeek;
+        private Week memNewWeek;
+        private Semester memOldSemester;
+        private Semester memNewSemester;
+        private int memCurrentWeek;
+        private int memNumWeeks;
+
+        //Event for user interface to subscribe to. 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Method for implementing the PropertyChanged Event. 
+        void LogicEngine_SubscriptionUpdate(object sender, PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, e);
+        }
+
+        /// <summary>
+        /// Default contructor for the LogicEngine class;
+        /// </summary>
+        public LogicEngine()
+        {
+            memOldWeek = new Week();
+            memNewWeek = new Week();
+            memCurrentWeek = 1;
+            memNumWeeks = 0;
+        }
+
+        //Setter for the Semesters
+        public void Semesters(DateTime OldSemesterStart, DateTime NewSemesterStart,  int NumWeeks, bool Breaks)
+        {
+            memOldSemester = new Semester(OldSemesterStart, NumWeeks, Breaks);
+            memNewSemester = new Semester(NewSemesterStart, NumWeeks, Breaks);
+            LogicEngine_SubscriptionUpdate(this, new PropertyChangedEventArgs("Semester Update"));
+        }
+
+        public Week OldWeek
+        {
+            get { return memOldWeek; }
+        }
+
+        public Week NewWeek
+        {
+            get { return memNewWeek; }
+        }
+
+        public void Next()
+        {
+            if(memCurrentWeek < memNumWeeks)
+            {
+                memCurrentWeek++;
+                memNewWeek = memNewSemester.Weeks[memCurrentWeek];
+                LogicEngine_SubscriptionUpdate(this, new PropertyChangedEventArgs("Next"));
+            }
+           
+        }
+
+        public void Previous()
+        {
+            if (memCurrentWeek > 1)
+            {
+                LogicEngine_SubscriptionUpdate(this, new PropertyChangedEventArgs("Previous"));
+            }
+        }
+
+        //Items Needed - Collection of Weeks
+        //Break Class that inherits from Week
+        //Pass weeks to userinterface to allow containers for the weeks that are efficiently passed to 
+        //the User Interface
+        //Use events to pass updates to the user interface. 
+
+
+
+    }
+}
