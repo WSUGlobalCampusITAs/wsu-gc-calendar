734a4e1b6442d4a5d68c656fffcba8a6bed03c7c
davidlystad <david.lystad@email.wsu.edu>
2015-03-06 16:49:26 -0800

    Building from the ground up into an MVVM architecture. So far have created the DataAccess, the Models, and started the Views.

---
+++ Calendar Converter/Calendar Converter/Model/Break.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Calendar_Converter.Model
+{
+    public class Break : Week
+    {
+        protected string memBreakName;
+
+        public static Break CreateWeek(DateTime Start, string BreakName)
+        {
+            return new Break { memBreakName = BreakName, memStart = Start, memisBreak = true };
+        }
+    }
+}
---
+++ Calendar Converter/Calendar Converter/Model/Week.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Calendar_Converter.Model
+{
+    public class Week
+    {
+        protected DateTime memStart;
+        protected bool memisBreak;
+
+        public static Week CreateWeek(DateTime Start)
+        {
+            return new Week { memStart = Start };
+        }
+
+        public DateTime Start
+        {
+            get { return memStart; }
+        }
+
+        public DateTime End
+        {
+            get { return memStart.AddDays(6); }
+        }
+
+    }
+}
